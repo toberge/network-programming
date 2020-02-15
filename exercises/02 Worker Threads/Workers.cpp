41e06a1edc4162282f330f619a5cd30adc463832
toberge <toberge@stud.ntnu.no>
2020-02-15 11:23:37 +0100

    Added some error handling and moved worker thread startup from constructor to start() method, in accordance with specifications.

--- exercises/02 Worker Threads/Workers.cpp
+++ exercises/02 Worker Threads/Workers.cpp
@@ -13,7 +13,8 @@ using namespace std;
  */
 class Workers {
     private:
-    bool debug;
+    const bool debug;
+    const int num_threads;
     // Thread storage
     vector<thread> thread_pool;
     // Locked by task_mutex
@@ -24,11 +25,43 @@ class Workers {
     condition_variable task_cv;
 
     public:
+    // Create a given number of worker threads (set debug to true if desired)
+    Workers(int num_threads, bool debug) : debug(debug), num_threads(num_threads) {
+        // Note to self: Initializers must be called in same order as the members are written
+        if (num_threads < 1) {
+            cerr << "Invalid number of threads" << endl;
+        }
+        //this->debug = debug;
+        //this->num_threads = num_threads;
+    }
+
     // Create a given number of worker threads
-    Workers(int num_threads, bool should_debug) {
-        debug = should_debug;
+    Workers(int num_threads) : Workers(num_threads, false) {}
+
+    // Add task to queue and signal to one worker thread
+    void post(function<void()> task) {
+        {
+            // Lock and add task
+            unique_lock<mutex> lock(task_mutex);
+            // Check if threads have been stopped
+            if (should_stop) {
+                cerr << "Cannot add work when stop() has been called" << endl;
+            }
+            tasks.emplace_back(task);
+        }
+        // Notify to one waiting thread that there's more work available
+        task_cv.notify_one();
+    }
+
+    // Called *once* to start all worker threads
+    void start() {
+        if (!thread_pool.empty()) {
+            cerr << "Workers::start() cannot be called more than once!" << endl;
+            return;
+        }
+
         for (int i = 0; i < num_threads; i++) {
-            // just add thread lambda directly
+            // Add thread lambda directly
             thread_pool.emplace_back([this, i] {
                 // Loop forever - until threads are explicitly stopped
                 while (true)
@@ -63,28 +96,23 @@ class Workers {
         }
     }
 
-    // Delegating constructor for non-debug mode
-    Workers(int num_threads) : Workers(num_threads, false) {}
-
-    // Add task to queue and signal to one worker thread
-    void post(function<void()> task) {
-        {
-            unique_lock<mutex> lock(task_mutex);
-            tasks.emplace_back(task);
-        }
-        task_cv.notify_one();
-    }
-
-    // Wait for all threads to finish then stop & join 'em
+    // Wait for all threads to finish then stop and join them
     void stop() {
         // TODO stop threads
         {
             unique_lock<mutex> lock(task_mutex);
+            // Do not proceed if stop flag already is set
+            if (should_stop) {
+                cerr << "Workers::stop() should only be called once!" << endl;
+                return;
+            }
+            // Set stop flag
             should_stop = true;
             if (debug) cout << "Notified all threads" << endl;
         }
         // Broadcast to all threads
         task_cv.notify_all();
+        // Join all threads - that is, wait for them to finish
         for (auto &thread : thread_pool) {
             thread.join();
         }
@@ -95,6 +123,8 @@ class Workers {
 int main() {
     Workers workers(10, true);
     Workers event_loop(1);
+
+    workers.start();
 
     // Add some work
     for (int i = 0; i < 40; i++) {
@@ -107,6 +137,9 @@ int main() {
     event_loop.post([] {
         cout << "Hello from event loop" << endl;
     });
+
+    event_loop.start();
+    event_loop.stop();
 
     this_thread::sleep_for(10s);
 
