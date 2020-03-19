11a615c50a521c77456218b9f19ded91774ee818
toberge <toberge@stud.ntnu.no>
2020-03-19 17:55:00 +0100

    Fix async server (plz improve soon)

--- exercises/05 UDP and TLS/async/server.py
+++ exercises/05 UDP and TLS/async/server.py
@@ -3,28 +3,42 @@ from socket import socket, SOCK_DGRAM, AF_INET
 # sys.path.append('../04 TCP Sockets')
 from calculator import process
 import asyncio
+import functools
 
-async def handle(data, remote):
+async def handle(sock, data, remote):
     expression = str(data, encoding='UTF-8')
         # if expression == '': continue
     try:
         result = process(expression)
         print('Resolved', expression.rstrip(), 'to', result)
-        socket.sendto(bytes('= ' + str(result) + '\n', encoding='UTF-8'), remote)
+        sock.sendto(bytes('= ' + str(result) + '\n', encoding='UTF-8'), remote)
     except Exception as error:
         result = str(error)
-        socket.sendto(bytes('ERROR: ' + str(error) + '\n', encoding='UTF-8'), remote)
+        sock.sendto(bytes('ERROR: ' + str(error) + '\n', encoding='UTF-8'), remote)
 
 ADDRESS = ('', 8080)
+
+# From https://stackoverflow.com/questions/41063331/how-to-use-asyncio-with-existing-blocking-library
+# TODO: write yer own thing, yo...
+def run_in_executor(f):
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
+    return inner
+
+@run_in_executor
+def recvfrom(sock, num):
+    return sock.recvfrom(num)
 
 async def main():
     with socket(AF_INET, SOCK_DGRAM) as sock:
         #sock.setblocking(False)
         sock.bind(ADDRESS)
         while True:
-            data, remote = sock.recvfrom(1024)
+            data, remote = await recvfrom(sock, 1024)
             # but recvfrom() is blocking...
-            loop.create_task(handle(data, remote))
+            loop.create_task(handle(sock, data, remote))
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
