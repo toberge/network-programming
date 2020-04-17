66931afd9f4f0ad5888416dbe5ac955d1ad9b723
toberge <toberge@stud.ntnu.no>
2020-04-17 13:20:07 +0200

    Make websocket close when a close frame is received

--- exercises/07 WebSocket/README.md
+++ exercises/07 WebSocket/README.md
@@ -25,3 +25,27 @@ Run `npm install` and `npm start` to start the server.
 Open [localhost:3000](http://localhost:3000)
 or `<your ip on the local network>:3000`
 in your browser.
+
+## Tests
+
+The library should reject the handshake if presented with a request that contains an _empty_ key:
+```bash
+curl \
+    -H 'Connection: Upgrade' \
+    -H 'Sec-WebSocket-Key: ' \
+    localhost:3001
+```
+or if the request does not have all the required headers:
+```bash
+curl -H 'Connection: Upgrade' localhost:3001
+curl -H 'Sec-WebSocket-Key: someverylegalkey' localhost:3001
+curl localhost:3001
+```
+
+It will close the connection if presented with a close frame.  
+This can be tested by typing
+```js
+ws.close()
+```
+in your web browser's console, when visiting the hosted webpage.  
+The server's log should now state that the connection has been closed.
--- exercises/07 WebSocket/whereissock.js
+++ exercises/07 WebSocket/whereissock.js
@@ -47,6 +47,8 @@ Sec-WebSocket-Accept: ${encoded}
      */
     __handleHandshake = (data, connection) => {
         let strings = data.toString().split('\r\n');
+        console.log(strings);
+        
         if (strings[0] === 'GET / HTTP/1.1') {
             console.log('Incoming connection!');
             // after handshake
@@ -63,15 +65,20 @@ Sec-WebSocket-Accept: ${encoded}
                     valid = true;
                 }
             }
+
+            // fail if no key or invalid header set
             if (!key || !valid) {
-                console.error('omg no key or invalid headers');
-                this.onerror(new Error('No key or handshake otherwise invalid'));
+                this.onerror('No key or handshake otherwise invalid');
+                connection.write(Buffer.from(`HTTP/1.1 400 Bad Request
+
+`));
+                connection.destroy();
                 return;
             }
-            //const key = 'x3JJHMbDL1EzLkh9GBhXDw==';
+
             this.clients.push({connection: connection, buffer: []});
-            const rsponse = this.__formHeader(key);
-            connection.write(rsponse);
+            const response = this.__formHeader(key);
+            connection.write(response);
             // upgrade status of this connection -> change listener!
             this.onconnect();
             connection.on('data', data => {
@@ -83,13 +90,14 @@ Sec-WebSocket-Accept: ${encoded}
             connection.write(Buffer.from(`HTTP/1.1 400 Bad Request
 
 `));
+        
         }
     }
 
     __handleMessage = (data) => {
         // TODO parse client message properly!
         for (let client of this.clients) {
-            const {buffer} = client;
+            const {connection, buffer} = client;
             const firstByte = data.readUInt8();
             const secondByte = data.readUInt8(1);
             const opcode = firstByte & (1+2+4+8);
@@ -99,26 +107,50 @@ Sec-WebSocket-Accept: ${encoded}
                 // against protocol 
                 // at least one of RSV1, RSV2, RSV3 set
                 // OR no mask indicated - client messages are always masked.
-                // OR opcode not 1 - not a plaintext msg
-                console.error('Client sent a malformet packet');
+                this.onerror('Client sent a malformet packet');
                 return;
                 // remove...
             }
-            if (!(opcode === 1)) {
-                console.error('Client sent a non-plaintext packet');
-                return;
-            }
 
-            const message = this.__decodeMessage(data);
-            // check if FIN is set
-            if (firstByte & 128) {
-                console.log('Client sent:', message);
-                this.onmessage(message);
-            } else {
-                // then this is NOT the first byte in the message.
-                // TODO handle this. somehow. idk how atm.
-                // add to buffer
-                buffer.push(message);
+            console.log(firstByte.toString(16))
+            console.log('Opcode was', opcode)
+
+            switch(opcode) {
+                case 1: {
+                    // text message
+                    let message = this.__decodeMessage(data);
+                    // check if FIN is set
+                    if (firstByte & 128) {
+                        // final package in message, good to go.
+                        if (buffer.length) {
+                            // there were more parts of the message!
+                            console.log('aha longer yes')
+                            message = [...buffer, message].join('')
+                        }
+                        console.log('Client sent:', message);
+                        this.onmessage(message);
+                    } else {
+                        // then this is NOT the first byte in the message.
+                        // this is an attempt at handling this case.
+                        buffer.push(message);
+                    }
+                    break;
+                }
+                case 8: {
+                    // Comply with this close request
+                    const message = this.__decodeMessage(data);
+                    console.log('Received close request:', message);
+                    // send our own close frame
+                    const response = this.__encodeMessage('Closing as requested', true);
+                    connection.write(response);
+                    // destroy connection
+                    connection.removeAllListeners();
+                    connection.destroy();
+                    break;
+                }
+                default:
+                    this.onerror('Unsupported opcode');
+                    break;
             }
         }
     }
@@ -138,15 +170,16 @@ Sec-WebSocket-Accept: ${encoded}
         return message;
     }
 
-    __encodeMessage = message => {
+    __encodeMessage = (message, close=false) => {
         let msgBuffer = Buffer.from(message);
         let length = msgBuffer.length;
         if (length > 127) {
             // TODO this *can* be handled but OH WELL
             console.error('message too long....');
         }
-        // temp
-        return Buffer.from([0x81, length, ...msgBuffer]);
+        // send opcode 8 if close=true
+        // otherwise opcode 1 for txt msg
+        return Buffer.from([(close? 0x88 : 0x81), length, ...msgBuffer]);
     }
 
     listen = (port = 3001, callback) =>
